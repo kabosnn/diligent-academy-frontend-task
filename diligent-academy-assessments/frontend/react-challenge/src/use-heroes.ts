631620075f740f805651d77b89ef3e97ee5c7bad
kabosnn <anagynandor@gmail.com>
2024-09-01 21:01:10 +0200

    add use heroes custom hook

---
+++ diligent-academy-assessments/frontend/react-challenge/src/use-heroes.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { callApi } from "./call-api";
+
+interface Hero {
+  id: number;
+  name: string;
+  available: boolean;
+}
+
+export const useHeroes = () => {
+  const [heroes, setHeroes] = useState<Hero[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchHeroes = async () => {
+      try {
+        const data = await callApi<Hero[]>("heroes");
+        setHeroes(data);
+        setLoading(false); 
+      } catch (err) {
+        setError(true);
+        setLoading(false); 
+      }
+    };
+    fetchHeroes();
+  }, []);
+
+  const toggleAvailability = (id: number) => {
+    setHeroes((prevHeroes) =>
+      prevHeroes.map((hero) =>
+        hero.id === id ? { ...hero, available: !hero.available } : hero
+      )
+    );
+  };
+
+  return { heroes, loading, error, toggleAvailability };
+};
