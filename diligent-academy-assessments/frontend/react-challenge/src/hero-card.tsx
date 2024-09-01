2ba639eaff041400288962701f6e9f882d368401
kabosnn <anagynandor@gmail.com>
2024-09-01 20:55:41 +0200

    add hero card component

---
+++ diligent-academy-assessments/frontend/react-challenge/src/hero-card.tsx
@@ -0,0 +1,33 @@
+interface Hero {
+  id: number;
+  name: string;
+  available: boolean;
+}
+
+interface HeroCardProps {
+  hero: Hero;
+  onToggle: (id: number) => void;
+}
+
+export default function HeroCard({ hero, onToggle }: HeroCardProps) {
+  const handleClick = () => {
+    onToggle(hero.id);
+  };
+
+  return (
+    <li
+      onClick={handleClick}
+      style={{
+        color: hero.available ? "green" : "red",
+        cursor: "pointer",
+        listStyle: "none",
+        display: "inline-block",
+        marginRight: "20px",
+      }}
+    >
+      {hero.id}. {hero.name} {hero.available ? '"Available"' : ""}
+    </li>
+  );
+}
+
+
