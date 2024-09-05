7e9f1fb9eda58ad3a7eff45a7a76e2e87fe2ee06
kabosnn <anagynandor@gmail.com>
2024-09-05 13:00:11 +0200

    add padding to div

--- diligent-academy-assessments/frontend/react-challenge/src/app.tsx
+++ diligent-academy-assessments/frontend/react-challenge/src/app.tsx
@@ -3,7 +3,9 @@ import HeroesList from "./heroes-list";
 function App() {
 
   return (
-    <div>
+    <div style={{
+      padding: '2rem'
+    }}>
       <h1>App</h1>
       <HeroesList />
     </div>
